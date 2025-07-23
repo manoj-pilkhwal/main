c59e076ea836f87af38d6c09f622c21e090a78b0
manoj-pilkhwal <manojpilkhwal6@gmail.com>
2025-07-23 16:54:14 +0530

    Added mongoose dependency

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid'); // to generate unique shift_id
+//const { v4: uuidv4 } = require('uuid'); // to generate unique shift_id
 
 const app = express();
 app.use(express.json());
